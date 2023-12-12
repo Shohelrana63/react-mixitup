911dfc1abd9ff99931d822d192263ee6254f114f
Shohelrana63 <shohelrana.beg@gmail.com>
2023-12-12 16:52:01 +0600

    raw mixitup is working cool

---
+++ src/GalleryData.js
@@ -0,0 +1,42 @@
+export const GalleryData = [
+  {
+    id: 1,
+    tittle: "car",
+    image: "./images/a1.jpg",
+  },
+  {
+    id: 2,
+    tittle: "car",
+    image: "./images/a2.jpg",
+  },
+  {
+    id: 3,
+    tittle: "car",
+    image: "./images/a3.jpg",
+  },
+  {
+    id: 4,
+    tittle: "car",
+    image: "./images/a4.jpg",
+  },
+  {
+    id: 5,
+    tittle: "bike",
+    image: "./images/b1.jpg",
+  },
+  {
+    id: 6,
+    tittle: "bike",
+    image: "./images/b2.jpg",
+  },
+  {
+    id: 7,
+    tittle: "cycle",
+    image: "./images/c1.jpg",
+  },
+  {
+    id: 8,
+    tittle: "cycle",
+    image: "./images/c2.jpg",
+  },
+];
